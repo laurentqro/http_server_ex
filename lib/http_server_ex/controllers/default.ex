200b5a01339ce3ca20fa59ff4485f53996e76dad
Laurent Curau <laurentcurau@gmail.com>
2018-10-25 17:07:29 +0100

    Remove now obsolete default controller

--- lib/http_server_ex/controllers/default.ex
+++
@@ -1,9 +0,0 @@
-defmodule HttpServerEx.Controllers.Default do
-
-  def process(conn) do
-    %{ conn |
-      resp_headers: %{ "Allow" => "GET, HEAD, OPTIONS, PUT, DELETE" },
-      status: 200
-    }
-  end
-end
